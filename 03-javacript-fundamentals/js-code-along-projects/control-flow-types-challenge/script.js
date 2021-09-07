fc040e6f96d2ba96eb31e97547c442aaa3e4d8fd
Nathan <nathanbeers@hotmail.co.uk>
2021-09-07 11:31:42 +0100

    Stylised and add control flow challenge to linking webpage

--- 03-javacript-fundamentals/js-code-along-projects/control-flow-types-challenge/script.js
+++ 03-javacript-fundamentals/js-code-along-projects/control-flow-types-challenge/script.js
@@ -1,3 +1,19 @@
+// Side nav
+
+function openNav() {
+    document.getElementById("Sidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("Sidenav").style.width = "0"
+}
+
+// Checks messages button
+
+function handleCheckMessages(event) {
+    alert("No new messages!")
+}
+
 // Odd or even number checker
 
 function handleCheckOddOrEven(event) {
