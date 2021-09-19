aaa6f6045a2b710cf86fcacdc8eeafbe2899bb77
Nathan <nathanbeers@hotmail.co.uk>
2021-09-19 17:15:12 +0100

    Completed array codealong

---
+++ 03-javacript-fundamentals/js-code-along-projects/arrays-code-along/script.js
@@ -0,0 +1,11 @@
+// 1. Create a coaches array
+const coaches = ["Andy", "Sam", "Bex", "Ashlee", "Jake"];
+console.log(coaches[1]);
+
+// 2. Create a array of pets
+const pets = ["Misty", "Snowy", "Archie", "Fenton"];
+console.log(pets[2]);
+
+// 3. Create a array of ages
+const ages = [20, 25, 28, 50];
+console.log(ages[3]);
