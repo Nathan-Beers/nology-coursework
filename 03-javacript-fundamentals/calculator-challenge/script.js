cc1a0f8e4efb1dc1cc71b75b83ca9da7bdc6cb88
Nathan <nathanbeers@hotmail.co.uk>
2021-10-10 11:48:49 +0100

    Created function to get and display firstNumber input

--- 03-javacript-fundamentals/calculator-challenge/script.js
+++ 03-javacript-fundamentals/calculator-challenge/script.js
@@ -3,6 +3,16 @@ let add = false;
 let minus = false;
 let times = false;
 let div = false;
-let equation = [firstNumber, secondNumber];
 let firstNumber = 0;
-let secondNumber = 0;
+let secondNumber = document.getElementById("secondNumber");
+let equation = [firstNumber, secondNumber];
+let submit = document.querySelector("submit");
+let equationDisplay = [];
+let equationDisplayEl = document.getElementById("equationDisplay-el")
+
+
+let getFirstNumber = () => {
+        displayEl.innerHTML = document.getElementById("firstNumber").value;
+        equationDisplayEl.innerHTML = document.getElementById("firstNumber").value;
+};
+
