62ad70afd065ee48833f5a4127c43315fa41051d
Nathan <nathanbeers@hotmail.co.uk>
2021-10-11 11:44:55 +0100

    Created function to get second number input

--- 03-javacript-fundamentals/calculator-challenge/script.js
+++ 03-javacript-fundamentals/calculator-challenge/script.js
@@ -7,19 +7,28 @@ let firstNumber = 0;
 let secondNumber = 0;
 let submit = document.querySelector("submit");
 let equationDisplay = [];
-let equationDisplayEl = document.getElementById("equationDisplay-el")
+let equationDisplayEl = document.getElementById("equationDisplay-el");
+let operator = "";
 
 
 let getFirstNumber = () => {
         displayEl.innerHTML = document.getElementById("firstNumber").value;
-        equationDisplayEl.innerHTML = document.getElementById("firstNumber").value;
         firstNumber = parseInt(document.getElementById("firstNumber").value, 10);
+        equationDisplayEl.innerHTML = firstNumber;
 };
 
 let getOperator = (event) => {
-        let operator = event.target.value;
+        operator = event.target.value;
         displayEl.innerHTML = operator;
         equationDisplayEl.innerHTML = firstNumber + operator;
 };
+
+let getSecondNumber = () => {
+        displayEl.innerHTML = document.getElementById("secondNumber").value;
+        secondNumber = parseInt(document.getElementById("secondNumber").value, 10);
+        equationDisplayEl.innerHTML = firstNumber + operator + secondNumber;
+}
+
+
 
 
