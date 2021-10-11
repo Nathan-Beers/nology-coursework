2b20a06bab64379f73a0366591bb0de9c4ed36af
Nathan <nathanbeers@hotmail.co.uk>
2021-10-11 19:52:23 +0100

    add pointer cursor to cancel button

--- 03-javacript-fundamentals/calculator-challenge/script.js
+++ 03-javacript-fundamentals/calculator-challenge/script.js
@@ -32,13 +32,19 @@ let getSecondNumber = () => {
         equationDisplayEl.innerHTML = firstNumber + operator + secondNumber;
 }
 
+let invalid = () => {
+        let inputs = document.getElementsByClassName("numberInputs");
+        
+}
+
 let calculate = () => {
         let equation = [firstNumber, operator, secondNumber];
         
         
 
         if(firstNumber === null || operator === "" || secondNumber === null){
-                alert("Please enter two numbers and select an operator")
+                alert("Please enter two numbers and select an operator");
+                invalid();
         } else if (typeof firstNumber == "number" && operator === "" && typeof secondNumber == "number") {
                 alert("Please select an operator");
         } else {
--- 03-javacript-fundamentals/js-code-along-projects/objects/main.js
+++ 03-javacript-fundamentals/js-code-along-projects/objects/main.js
@@ -1,32 +1,46 @@
-// OBJECTS SYNTAX
+// // OBJECTS SYNTAX
 
-const exampleObject = {
-    key: "value", // COMMA FOR OTHER KEYS
-    types: "any",
-    method() {
-        // DO SOMETHING?
-        // this.key
-        // RETRUNS SOMETHING?
+// const exampleObject = {
+//     key: "value", // COMMA FOR OTHER KEYS
+//     types: "any",
+//     method() {
+//         // DO SOMETHING?
+//         // this.key
+//         // RETRUNS SOMETHING?
 
-        // "this" keyword refers to the object
-        return this.key + " " + this.types
+//         // "this" keyword refers to the object
+//         return this.key + " " + this.types
+//     }
+// }
+
+// // ACCESS KEYS
+// // DOT NOTATION
+
+// console.log(exampleObject.key)
+// console.log(exampleObject.types)
+
+// // CALLING A METHOD
+// console.log(exampleObject.method())
+
+// // SQUARE BRACKETS
+// console.log(exampleObject["key"])
+// console.log(exampleObject["types"])
+
+// // CREATE NEW KEYS
+
+// exampleObject.newKey = "new value";
+// console.log(exampleObject.newKey)
+
+const person = {
+    firstName: "Nathan",
+    lastName: "Beers",
+    getFullName(){
+        return `${this.firstName} ${this.lastName}`
     }
 }
 
-// ACCESS KEYS
-// DOT NOTATION
+person.age = 25;
+console.log(`Hi my name is ${person.firstName} ${person.lastName}!`)
+console.log(person)
 
-console.log(exampleObject.key)
-console.log(exampleObject.types)
-
-// CALLING A METHOD
-console.log(exampleObject.method())
-
-// SQUARE BRACKETS
-console.log(exampleObject["key"])
-console.log(exampleObject["types"])
-
-// CREATE NEW KEYS
-
-exampleObject.newKey = "new value";
-console.log(exampleObject)
+console.log(person.getFullName())
