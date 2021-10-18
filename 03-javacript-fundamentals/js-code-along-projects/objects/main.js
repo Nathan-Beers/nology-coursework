940ffff6fb9e0d84b139367397a665d10065ade7
Nathan <nathanbeers@hotmail.co.uk>
2021-10-18 15:41:25 +0100

    objects codealong mini challenge

--- 03-javacript-fundamentals/js-code-along-projects/objects/main.js
+++ 03-javacript-fundamentals/js-code-along-projects/objects/main.js
@@ -34,13 +34,26 @@
 const person = {
     firstName: "Nathan",
     lastName: "Beers",
+    quote: "When you get tired, learn to rest, not quit.",
+    skills: ["html", "css", "javascript", "lua"],
     getFullName(){
         return `${this.firstName} ${this.lastName}`
+    },
+    getSkillsHTML() {
+        const skillsHTML = this.skills.map(skill => `<li>${skill}</li>`)
+        console.log(skillsHTML);
+        return skillsHTML.join("\n");
     }
 }
 
-person.age = 25;
-console.log(`Hi my name is ${person.firstName} ${person.lastName}!`)
-console.log(person)
+// WRITE A METHOD ON THE PERSON OBJECT THAT USES THE SKILLS ARRAY
+// ITERATE OVER THE SKILLS ARRAY AND GENERATE A LIST ITEM FOR EACH SKILL
+// E.G: <li>html</li>
+// NEED TO RETURN A STRING -> ARRAY INTO A STRING
 
-console.log(person.getFullName())
+person.age = 25;
+
+// console.log(`Hi my name is ${person.firstName} ${person.lastName}!`)
+console.log(person.getSkillsHTML());
+
+// console.log(person.getFullName())
