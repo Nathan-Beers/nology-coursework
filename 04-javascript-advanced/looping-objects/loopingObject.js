85747cd227eaa9faa99da630f317c935e0afd06e
Nathan <nathanbeers@hotmail.co.uk>
2021-11-29 10:47:39 +0000

    Values/keys from objects part 1

--- 04-javascript-advanced/looping-objects/loopingObject.js
+++ 04-javascript-advanced/looping-objects/loopingObject.js
@@ -1,17 +1,17 @@
-const message = {
+const validMessage = {
     userName : "Nathan",
     content : "Have a great day!",
 };
 
-const greeting = {
+const invalidMessage = {
     userName : "Charlie",
     content : 2531313,
 };
 
-for (let key in message) {
-    console.log(key);
-    console.log(message[key]);
-}
+// for (let key in message) {
+//     console.log(key);
+//     console.log(message[key]);
+// }
 
 // Create a function that loops through a message object
 // Check that all of values are strings
@@ -29,5 +29,27 @@ const isMessageObjectString = (messageObject) => {
     return true;
 };
 
-console.log(isMessageObjectString(message));
-console.log(isMessageObjectString(greeting));
+// console.log(isMessageObjectString(message));
+// console.log(isMessageObjectString(greeting));
+
+// const validMessageValues = Object.values(validMessage);
+// const invalidMessageValues = Object.values(invalidMessage);
+
+// console.log(validMessageValues);
+// console.log(invalidMessageValues);
+
+// console.log(validMessageValues.every(validMessage => {
+//     const typeCheck = typeof validMessage === "string";
+//     return typeCheck;
+// }));
+
+// console.log(invalidMessageValues.every(invalidMessage => {
+//     const typeCheck = typeof invalidMessage === "string";
+//     return typeCheck;
+// }));
+
+const validMessageKeys = Object.keys(validMessage);
+const invalidMessageKeys = Object.keys(invalidMessage);
+
+console.log(validMessageKeys);
+console.log(invalidMessageKeys);
