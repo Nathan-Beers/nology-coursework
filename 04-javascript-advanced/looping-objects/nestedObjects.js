1d6c5d869b2a458419862c8e7be9e5c0fcffd907
Nathan <nathanbeers@hotmail.co.uk>
2021-11-30 11:49:02 +0000

    nested objects part 2

--- 04-javascript-advanced/looping-objects/nestedObjects.js
+++ 04-javascript-advanced/looping-objects/nestedObjects.js
@@ -4,7 +4,8 @@ const user = {
     information: {
         firsName: "Nathan",
         lastName: "Beers",
-        age: 25
+        age: 25,
+        likes: ["drawing", "running", "baking"]
     },
     contact: {
         email: "nathan@nathan.co.uk",
@@ -15,9 +16,47 @@ const user = {
             postcode: "N18 5PS",
             country: "UK"
         }
-    }
-}
+    },
+    
+};
 
-console.log(user.information.age);
-console.log(user.contact.address.nameOfRoad);
-console.log(user.contact.contactNumber)
+const userTwo = {
+    id: "asdf123000",
+    loggedIn: true,
+    information: {
+        firsName: "Nathan",
+        lastName: "Beers",
+        age: 25,
+        likes: ["drawing", "running", "baking"]
+    },
+    contact: {
+        email: "nathan@nathan.co.uk",
+        contactNumber: "077579437283",
+        address: {
+            houseNumber: 30,
+            nameOfRoad: "Couch End",
+            postcode: "N18 5PS",
+            country: "UK"
+        }
+    },
+    
+};
+
+// console.log(user.information.age);
+// console.log(user.contact.address.nameOfRoad);
+// console.log(user.contact.contactNumber)
+
+// age
+// console.log(user.information.age);
+// // country
+// console.log(user.contact.address.country);
+// // return whole contact object
+// console.log(user.contact);
+
+// console.log(user.information.likes[1])
+
+const userArray = [user, userTwo];
+
+userArray.forEach(user => {
+    console.log(user.information.likes)
+})
