0f4d2c4131e89ebd975a0776750537ca6a3a51e6
Nathan <nathanbeers@hotmail.co.uk>
2022-01-04 11:48:47 +0000

    complete refractoring

--- 04-javascript-advanced/pure-functions/main.js
+++ 04-javascript-advanced/pure-functions/main.js
@@ -1,13 +1,36 @@
-// Attached event listener to button
-document.querySelector('.translate-btn').addEventListener('click', () => {
-    const englishWord = document.querySelector('.input-box').value;
+// // Attached event listener to button
+// document.querySelector('.translate-btn').addEventListener('click', () => {
+//     const englishWord = document.querySelector('.input-box').value;
     
-    // If word matches, put translation into DOM
-    if(englishWord === "Hello" || englishWord === "hello"){
-        document.querySelector('.translate-result').innerHTML = "Bonjour";
-    } else if (englishWord === "Goodbye" || englishWord === "goodbye") {
-        document.querySelector('.translate-result').innerHTML = "Au revoir";
+//     // If word matches, put translation into DOM
+//     if(englishWord === "Hello" || englishWord === "hello"){
+//         document.querySelector('.translate-result').innerHTML = "Bonjour";
+//     } else if (englishWord === "Goodbye" || englishWord === "goodbye") {
+//         document.querySelector('.translate-result').innerHTML = "Au revoir";
+//     } else {
+//         document.querySelector('.translate-result').innerHTML = "Translation unavailable";
+//     }
+// });
+
+// Make the code more testable
+// Decouple logic
+// 1. Function that translates english to french
+// 2. Some dom interatction in an even listener
+
+// Accept inputs => return outputs
+const translateEnglishToFrench = (englishWord) => {
+    if(englishWord === "hello") {
+        return "bonjour";
+    } else if(englishWord === "goodbye"){
+        return "au revoir";
     } else {
-        document.querySelector('.translate-result').innerHTML = "Translation unavailable";
+        return "unavailable";
     }
+};
+
+document.querySelector(".translate-btn").addEventListener("click", () => {
+    const englishWord = document.querySelector(".input-box").value;
+    const frenchWord = translateEnglishToFrench(englishWord);
+
+    document.querySelector(".translate-result").innerHTML = frenchWord;
 });
