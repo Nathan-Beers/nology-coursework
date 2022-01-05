b9bce80b77c9d26d0ee4b516414eba6e59566432
Nathan <nathanbeers@hotmail.co.uk>
2022-01-05 12:47:52 +0000

    user APIs challenge

--- 04-javascript-advanced/promises/app.js
+++ 04-javascript-advanced/promises/app.js
@@ -2,19 +2,42 @@
 // A promise has 3 states... pending, resolved and rejected
 
 
-let nathansPromise = new Promise((resolve, reject) => {
-    let a = 1 + 2;
-    if (a == 2) {
-        resolve("Success");
-    } else {
-        reject("Failed");
-    }
-});
+// let nathansPromise = new Promise((resolve, reject) => {
+//     let a = 1 + 2;
+//     if (a == 2) {
+//         resolve("Success");
+//     } else {
+//         reject("Failed");
+//     }
+// });
 
-nathansPromise
-    .then((message) => {
-        console.log(`${message} I am in the THEN`)
-    })
-    .catch((message) => {
-        console.log(`${message} I am in the CATCH`)
-    });
+// nathansPromise
+//     .then((message) => {
+//         console.log(`${message} I am in the THEN`)
+//     })
+//     .catch((message) => {
+//         console.log(`${message} I am in the CATCH`)
+//     });
+
+// API requests
+// https://randomuser.me/api/
+
+const heading = document.querySelector('.user');
+const userAge = document.querySelector('.age');
+const getRandomUser = () => {
+    fetch('https://randomuser.me/api/')
+        .then(res => res.json())
+        .then(json => heading.innerHTML = `${json.results[0].name.first} ${json.results[0].name.last}`)
+        .catch(err => console.log(err))
+};
+
+const getUserAge = () => {
+    fetch('https://randomuser.me/api/')
+        .then(res => res.json())
+        .then(json => userAge.innerHTML = `Age: ${json.results[0].registered.age}`)
+        .catch(err => console.log(err))
+}
+
+getRandomUser();
+getUserAge();
+
